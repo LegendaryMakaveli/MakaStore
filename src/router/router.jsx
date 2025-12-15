eea391201bf24a83b9a47081424e76f17eab92df
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-15 04:06:12 +0100

    add router to navigate through pages

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,10 +2,14 @@ import React from "react";
 import { createBrowserRouter } from "react-router";
 import Products from "../components/products.jsx";
 import LandingPage from "../pages/LandingPage.jsx";
+import SingleProduct from "../components/singleProduct.jsx";
+import CartPage from "../pages/cartPage.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <LandingPage /> },
   { path: "/products", element: <Products /> },
+  {path: "/products/:id", element: <SingleProduct />},
+  {path: "/cart", element: <CartPage />}
 ]);
 
 export default router;
