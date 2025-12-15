066e64438c01d880d89f95b718e1152d703e2c72
LegendaryMakaveli <adeyemoakorede93@gmail.com>
2025-12-15 04:08:01 +0100

    store for my redux

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {productApi} from "../apis/productApi";
+import cartReducer from "../features/cart/cartSlice";
 
 
 export const store = configureStore({
     reducer: {
         [productApi.reducerPath]: productApi.reducer,
+        cart: cartReducer
     },
     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(productApi.middleware)
 });
